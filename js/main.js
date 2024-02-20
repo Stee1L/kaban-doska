55ebad7de230f5434f882b8f7f4a9882dd8c17ef
mac <superstart8016@gmail.com>
2024-02-20 14:51:49 +0700

    добавление новой карточки задания и ее удаления

--- js/main.js
+++ js/main.js
@@ -2,14 +2,32 @@ new Vue({
     el: '#app',
     data() {
         return {
+            plannedTasks: [], // чертов массив
             newTask: {
                 title: '',
                 description: '',
                 deadline: '',
-                reatedAt: new Date().toLocaleString(),
-                lastEdited: null
+                createdAt: new Date().toLocaleString(),
+                lastEdited: null,
             },
+        };
+    },
+    methods: {
+        addTask() {
+            this.plannedTasks.push({
+                title: this.newTask.title,
+                description: this.newTask.description,
+                deadline: this.newTask.deadline,
+                createdAt: this.newTask.createdAt,
+                lastEdited: this.newTask.lastEdited,
+            });
 
-
-        }
-    }})
+            this.newTask.title = '';
+            this.newTask.description = '';
+            this.newTask.deadline = '';
+        },
+        deleteTask(taskIndex) {
+            this.plannedTasks.splice(taskIndex, 1);
+        },
+    },
+});
