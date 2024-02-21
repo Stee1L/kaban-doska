7fe5cf537a47aab5fd1ddf258863f1f14b30e2b9
mac <superstart8016@gmail.com>
2024-02-21 15:09:01 +0700

    работаем негры, солнышко еще светит

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ new Vue({
     data() {
         return {
             plannedTasks: [], // чертов массив
+            inProgressTasks: [],
             newTask: {
                 title: '',
                 description: '',
@@ -10,17 +11,31 @@ new Vue({
                 createdAt: new Date().toLocaleString(),
                 lastEdited: null,
             },
+            editingTaskIndex: -1,
         };
     },
     methods: {
         addTask() {
-            this.plannedTasks.push({
-                title: this.newTask.title,
-                description: this.newTask.description,
-                deadline: this.newTask.deadline,
-                createdAt: this.newTask.createdAt,
-                lastEdited: this.newTask.lastEdited,
-            });
+                if (this.editingTaskIndex !== -1) {
+                    // Редактирование
+                    this.plannedTasks.splice(this.editingTaskIndex, 1, {
+                        title: this.newTask.title,
+                        description: this.newTask.description,
+                        deadline: this.newTask.deadline,
+                        createdAt: this.newTask.createdAt,
+                        lastEdited: this.newTask.lastEdited,
+                    });
+                    this.editingTaskIndex = -1;
+                } else {
+                    this.plannedTasks.push({
+                        title: this.newTask.title,
+                        description: this.newTask.description,
+                        deadline: this.newTask.deadline,
+                        createdAt: this.newTask.createdAt,
+                        lastEdited: this.newTask.lastEdited,
+                    });
+
+                }
 
             this.newTask.title = '';
             this.newTask.description = '';
@@ -29,5 +44,31 @@ new Vue({
         deleteTask(taskIndex) {
             this.plannedTasks.splice(taskIndex, 1);
         },
+        startEditing(index) {
+            this.editingTaskIndex = index;
+            const taskToEdit = this.plannedTasks[index];
+
+
+            this.newTask.title = taskToEdit.title;
+            this.newTask.description = taskToEdit.description;
+            this.newTask.deadline = taskToEdit.deadline;
+        },
+        moveToInProgress(index) {
+
+            if (this.editingTaskIndex !== -1) {
+                console.error("Завершите редактирование перед перемещением.");
+                return;
+            }
+
+            // Получение задачи для перемещения
+            const taskToMove = this.plannedTasks[index];
+
+
+            this.plannedTasks.splice(index, 1);
+
+
+            this.inProgressTasks.push(taskToMove);
+        },
     },
+
 });
